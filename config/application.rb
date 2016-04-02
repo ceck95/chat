ec96db5a1c846413ceaaa15ef329a68635fd4f69
nhutuit <tranvannhut4495@gmail.com>
2016-04-02 13:51:43 +0700

    ok

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,6 @@ require File.expand_path('../boot', __FILE__)
 require File.expand_path('../csrf_protection', __FILE__)
 
 require 'rails/all'
-require 'faye/websocket'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
