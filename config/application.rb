90fd406b778b4d530853f2915e6749eeca682e27
nhutuit <tranvannhut4495@gmail.com>
2016-04-02 13:21:02 +0700

    update

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module Chat
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.middleware.delete Rack::Lock
-    config.middleware.use FayeRails::Middleware, mount: '/faye', :timeout => 25
+    faye_server = Faye::RackAdapter.new(:mount => '/faye', :timeout => 45)
     Faye::WebSocket.load_adapter('thin')
     faye_server.listen(80)
 
