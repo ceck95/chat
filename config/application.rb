b5d6da69222688bbde065b41ec31ff5f11b83237
nhutuit <tranvannhut4495@gmail.com>
2016-04-02 13:32:55 +0700

    ok

--- config/application.rb
+++ config/application.rb
@@ -37,35 +37,35 @@ module Chat
     #       ENV[key.to_s] = value
     #     end if File.exists?(env_file)
     #   end
+    # end
     app = lambda do |env|
-      if env['PATH_INFO'] == '/faye'
-        ws = Faye::WebSocket.new(env)
+  if env['PATH_INFO'] == '/faye'
+    ws = Faye::WebSocket.new(env)
 
-        timer = EM.add_periodic_timer(1) do
-          ws.send(Time.now.to_s)
-        end
-
-        ws.on :message do |event|
-          ws.send("You sent: #{event.data}")
-        end
-
-        ws.on :close do |event|
-          EM.cancel_timer(timer)
-          ws = nil
-        end
-
-        # Return async Rack response
-        ws.rack_response
-
-      else
-        [404, { "Content-Type" => "text/plain" }, ["Not found"]]
-      end
+    timer = EM.add_periodic_timer(1) do
+      ws.send(Time.now.to_s)
     end
 
-    # See https://www.phusionpassenger.com/library/config/tuning_sse_and_websockets/
-    if defined?(PhusionPassenger)
-      PhusionPassenger.advertised_concurrency_level = 0
+    ws.on :message do |event|
+      ws.send("You sent: #{event.data}")
     end
+
+    ws.on :close do |event|
+      EM.cancel_timer(timer)
+      ws = nil
     end
+
+    # Return async Rack response
+    ws.rack_response
+
+  else
+    [404, { "Content-Type" => "text/plain" }, ["Not found"]]
+  end
+end
+
+# See https://www.phusionpassenger.com/library/config/tuning_sse_and_websockets/
+if defined?(PhusionPassenger)
+  PhusionPassenger.advertised_concurrency_level = 0
+end
   end
 end
